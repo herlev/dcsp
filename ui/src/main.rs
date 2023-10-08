9f17f973da43b81afffe7e09597d26a18b87f33c
herlev <>
2023-10-08 17:39:40 +0200

    add rust-embed to load assets into the rust binary at compile time, to allow for deploying a single binary without any additional files

--- ui/src/main.rs
+++ ui/src/main.rs
@@ -1,33 +1,39 @@
+mod routes;
+mod static_assets;
+
 use anyhow::Context;
 use axum::{routing::get, Router};
-use std::str::FromStr;
-use tower_http::services::ServeDir;
+use static_assets::static_handler;
+use tower_http::trace::TraceLayer;
 use tower_livereload::LiveReloadLayer;
-use tracing::info;
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
-
-mod routes;
+use tracing::{info, Level};
+use tracing_subscriber::{filter, layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
   tracing_subscriber::registry()
-    .with(tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| "ui=debug".into()))
     .with(tracing_subscriber::fmt::layer())
+    .with(tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| "ui=debug".into()))
+    .with(
+      filter::Targets::new()
+        .with_target("tower_http::trace::on_response", Level::TRACE)
+        .with_target("tower_http::trace::on_request", Level::TRACE)
+        .with_target("tower_http::trace::make_span", Level::DEBUG)
+        .with_default(Level::INFO),
+    )
     .init();
-  info!("initializing router...");
-  let assets_path = std::env::current_dir().unwrap();
   let api_router = Router::new().route("/status", get(routes::api_status));
 
   let router = Router::new()
-    .nest("/api", api_router)
     .route("/", get(routes::status))
-    .nest_service(
-      "/assets",
-      ServeDir::new(format!("{}/assets", assets_path.to_str().unwrap())),
-    )
-    .layer(LiveReloadLayer::new());
-  let addr = std::net::SocketAddr::from_str("0.0.0.0:8000").unwrap();
-  axum::Server::bind(&addr)
+    .layer(LiveReloadLayer::new())
+    .nest("/api", api_router)
+    .route("/assets/*file", get(static_handler))
+    .layer(TraceLayer::new_for_http());
+  let port = std::env::var("PORT").unwrap_or("8000".into());
+  let addr = format!("0.0.0.0:{port}");
+  info!("Starting server on {addr}");
+  axum::Server::bind(&addr.parse().unwrap())
     .serve(router.into_make_service())
     .await
     .context("error while starting server")?;
---
+++ ui/src/static_assets.rs
@@ -0,0 +1,38 @@
+use axum::http::header;
+use axum::http::StatusCode;
+use axum::http::Uri;
+use axum::response::{IntoResponse, Response};
+use rust_embed::RustEmbed;
+
+pub async fn static_handler(uri: Uri) -> impl IntoResponse {
+  let mut path = uri.path().trim_start_matches('/').to_string();
+
+  if path.starts_with("assets/") {
+    path = path.replace("assets/", "");
+  }
+
+  StaticFile(path)
+}
+
+#[derive(RustEmbed)]
+#[folder = "assets"]
+struct Asset;
+
+pub struct StaticFile<T>(pub T);
+
+impl<T> IntoResponse for StaticFile<T>
+where
+  T: Into<String>,
+{
+  fn into_response(self) -> Response {
+    let path = self.0.into();
+
+    match Asset::get(path.as_str()) {
+      Some(content) => {
+        let mime = mime_guess::from_path(path).first_or_octet_stream();
+        ([(header::CONTENT_TYPE, mime.as_ref())], content.data).into_response()
+      }
+      None => (StatusCode::NOT_FOUND, "404 Not Found").into_response(),
+    }
+  }
+}
